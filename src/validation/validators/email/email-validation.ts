f0fc30d8cf9d64a982a3516643da4d52b0a9e07d
Hugo Oliveira <hbliveira@gmail.com>
2021-06-07 21:08:22 -0300

    fix: ensure email validation allows empty value

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,9 +4,9 @@ import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
-  validate(value: string): Error {
+  validate(value?: string): Error {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-    return emailRegex.test(value) ? null : new InvalidFieldError()
+    return !value || emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }
