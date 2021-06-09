91664696a8d4961c485708951b664d9717be08e7
Hugo Oliveira <hbliveira@gmail.com>
2021-06-08 21:23:30 -0300

    feature: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -0,0 +1,11 @@
+import {
+  RequiredFieldValidation,
+  ValidationBuilder as sut
+} from '@/validation/validators'
+
+describe('ValidationBuilder', () => {
+  test('Should return RequiredFieldValidation', () => {
+    const validations = sut.field('any_field').required().build()
+    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+})
--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,14 +1,15 @@
 import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
 import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation'
+import faker from 'faker'
 
 type SutTypes = {
   sut: ValidationComposite
   fieldValidationsSpy: FieldValidationSpy[]
 }
-const makeSut = (): SutTypes => {
+const makeSut = (fieldName: string): SutTypes => {
   const fieldValidationsSpy = [
-    new FieldValidationSpy('any_field'),
-    new FieldValidationSpy('any_field')
+    new FieldValidationSpy(fieldName),
+    new FieldValidationSpy(fieldName)
   ]
   const sut = new ValidationComposite(fieldValidationsSpy)
 
@@ -20,11 +21,19 @@ const makeSut = (): SutTypes => {
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const { sut, fieldValidationsSpy } = makeSut()
-    fieldValidationsSpy[0].error = new Error('first_error_message')
-    fieldValidationsSpy[1].error = new Error('second_error_message')
+    const { sut, fieldValidationsSpy } = makeSut(faker.database.column())
 
-    const error = sut.validate('any_field', 'any_value')
-    expect(error).toEqual('first_error_message')
+    const errorMessage = faker.random.words()
+    fieldValidationsSpy[0].error = new Error(errorMessage)
+    fieldValidationsSpy[1].error = new Error(faker.random.words())
+
+    const error = sut.validate('any_field', faker.random.words())
+    expect(error).toEqual(error)
+  })
+
+  test('Should return error if any validation fails', () => {
+    const { sut } = makeSut(faker.database.column())
+    const error = sut.validate('any_field', faker.random.words())
+    expect(error).toBeFalsy()
   })
 })
