6db9e9063867b267f11d61f88293203fe081d2e4
Hugo Oliveira <you@example.com>
2021-08-03 20:50:18 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 403

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -1,7 +1,7 @@
+import { HttpResponse } from '.'
 export type HttpGetParams = {
   url: string
 }
-
-export interface HttpGetClient {
-  get: (params: HttpGetParams) => Promise<void>
+export interface HttpGetClient<R = any> {
+  get: (params: HttpGetParams) => Promise<HttpResponse<R>>
 }
