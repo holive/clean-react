a361c85d0c6edbce6a56bd5d913ed369fd1aadc6
Hugo Oliveira <you@example.com>
2021-08-05 21:14:31 -0300

    refactor: change cache storage to sync

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -2,12 +2,18 @@ import * as faker from 'faker'
 import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
   test('Should call localStorage with correct values', async () => {
+    const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
-    await new LocalStorageAdapter().set(key, value)
-
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
