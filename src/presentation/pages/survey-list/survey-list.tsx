a58d7c06c03265c7b4e54d276d15033e4d076006
Hugo Oliveira <you@example.com>
2021-08-10 19:43:22 -0300

    refactor: move SurveyList code to components

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,9 @@
 import Styles from './survey-list-styles.scss'
 import { Header, Footer } from '@/presentation/components'
 import {
-  SurveyItemEmpty,
-  SurveyItem
+  SurveyContext,
+  SurveyListItem,
+  Error
 } from '@/presentation/pages/survey-list/components'
 import { LoadSurveyList } from '@/domain/usecases'
 import { SurveyModel } from '@/domain/models'
@@ -30,22 +31,9 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        {state.error ? (
-          <div>
-            <span data-testid='error'>{state.error}</span>
-            <button>Recarregar</button>
-          </div>
-        ) : (
-          <ul data-testid='survey-list'>
-            {state.surveys.length ? (
-              state.surveys.map((survey: SurveyModel) => (
-                <SurveyItem key={survey.id} survey={survey} />
-              ))
-            ) : (
-              <SurveyItemEmpty />
-            )}
-          </ul>
-        )}
+        <SurveyContext.Provider value={{ state, setState }}>
+          {state.error ? <Error /> : <SurveyListItem />}
+        </SurveyContext.Provider>
       </div>
       <Footer />
     </div>
