6c7d918b089e90217b4b70341ee05dfd722b677b
Hugo Oliveira <you@example.com>
2021-08-11 19:32:25 -0300

    refactor: change validation factory to not use builder

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,13 +1,20 @@
 import { makeLoginValidation } from '@/presentation/pages/login/login-validation-factory'
-import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
+import {
+  EmailValidation,
+  MinLengthValidation,
+  RequiredFieldValidation,
+  ValidationComposite
+} from '@/validation/validators'
 
 describe('', () => {
   test('Should make ValidationComposite with correct validations ', () => {
     const composite = makeLoginValidation()
     expect(composite).toEqual(
       new ValidationComposite([
-        ...ValidationBuilder.field('email').required().email().build(),
-        ...ValidationBuilder.field('password').required().min(5).build()
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5)
       ])
     )
   })
