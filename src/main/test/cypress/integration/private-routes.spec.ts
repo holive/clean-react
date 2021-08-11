b9fa2f8da13862dd1187336df36e0be52bc72774
Hugo Oliveira <you@example.com>
2021-08-11 20:15:38 -0300

    test: ensure SurveyList logout if no token is found

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -1,7 +1,7 @@
 import * as Helper from '../support/helpers'
 
 describe('Private Routes', () => {
-  it('Should logout if survey-list has no token', () => {
+  it('Should logout  if survey-list has no token', () => {
     cy.visit('')
     Helper.testUrl('/login')
   })
